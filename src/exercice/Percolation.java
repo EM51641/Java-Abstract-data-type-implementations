eeb9d91069d321f60be92e0993930f12be0a962d
Elyes <elyesmahjoubi@hotmail.com>
2022-09-22 17:19:24 +0200

    final version

---
+++ src/exercice/Percolation.java
@@ -0,0 +1,129 @@
+//package exercice;
+//import java.io.Console;
+//import java.lang.reflect.Array;
+//import java.util.Arrays;
+
+//import java.util.Arrays;
+
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class Percolation {
+    
+    private WeightedQuickUnionUF finderunionizer;
+    private int[][] array;
+    private int n;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n){
+
+        if(n <= 0){
+            throw new IllegalArgumentException("Impossible to initialise an array with a negative length");
+        }
+
+        this.n = n; 
+        this.array = new int[n][n]; // initialise the array
+        this.finderunionizer = new WeightedQuickUnionUF(n*n + 2); //initialise the index list while adding 2 virtual boxes at the beginning and the end of the array
+        this.connectVirtualBlock(); // connection the virtual boxes so as to limit the search operation to a O(n) complexity instead of an O(n^2)
+
+        while(!this.percolates() == true){
+            this.openrandomlyandjoin(); //open a random location and link it to the next open ones.
+        };
+    };
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col){
+        this.array[row][col] = 1; //open the location
+        join(row, col); //join the location
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col){
+
+        if (row  < this.n && row >= 0 && col < this.n && col >= 0){
+            if(this.array[row][col] == 1)return true;
+            else return false;
+        }
+        else{
+            throw new IllegalArgumentException("Out of bound index");
+        }  
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col){
+
+        if(this.array[row][col] == 0)return true;
+        else return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites(){
+    
+        int sum = 0;
+
+        for (int[] arr : this.array)
+            for(int i: arr)
+                sum+=i;
+    return sum ;
+    }  
+
+    // join the index
+    private void join(int row, int col){
+
+        if(col < this.n - 1 && this.isOpen(row, col + 1)){
+            this.finderunionizer.union( row * this.n + col + 1, row * this.n + (col + 1) + 1);
+        }
+        
+        if(col > 0 && this.isOpen(row, col - 1)){
+            this.finderunionizer.union( row * this.n + (col - 1) + 1, row * this.n + col + 1);
+        }
+        
+        if(row > 0 && this.isOpen(row - 1, col)){
+            this.finderunionizer.union( ( row - 1 ) * this.n + col + 1, row * this.n + col + 1);
+        }
+        
+        if(row < this.n - 1 && this.isOpen(row + 1, col)){
+            this.finderunionizer.union( row * (this.n) + col + 1, (row + 1) * this.n + col + 1);
+        } 
+  };
+
+    // does the system percolate?
+    public boolean percolates(){
+
+        if(this.finderunionizer.find(0) == this.finderunionizer.find(this.n * this.n + 1)){
+            return true;
+        }
+        else return false;   
+    }
+    // open a random case and make unions with other cases
+    private void openrandomlyandjoin(){
+        int row =  StdRandom.uniformInt(0, this.n);
+        int col = StdRandom.uniformInt(0, this.n);
+        this.open(row, col); 
+    }
+
+    private void visualize(){
+
+        for (int[] row : this.array){
+            System.out.println(Arrays.toString(row));
+    };
+};
+    
+    // connect virtual boxes at the 'top' and the 'bottom' of the array
+    private void connectVirtualBlock(){
+        for (int j = 0; j < this.n; j++){
+            this.finderunionizer.union( 0,  1 + j ); // top wire
+            this.finderunionizer.union(this.n * this.n + 1, this.n * (this.n - 1) + j + 1); //low wire
+        }
+    }
+
+    // test client (optional)
+    public static void main(String[] args){
+        Percolation percolate = new Percolation(-2);
+        percolate.visualize();
+    }
+        
+}
---
+++ src/exercice/PercolationStats.java
@@ -0,0 +1,64 @@
+//package exercice;
+
+
+import java.lang.Math;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+
+    private double[] arr;
+    private double n;
+
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials){
+
+        this.arr = new double[trials];
+        this.n = n;
+
+        for(int i = 0; i < trials; i++ ){
+            Percolation percolation = new Percolation(n);
+            int openCase = percolation.numberOfOpenSites();
+            this.arr[i] = openCase/(this.n * this.n);
+        }
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+        return StdStats.mean(this.arr);}
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+        return StdStats.stddev(this.arr);}
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo(){
+        return mean() - 1.96 * stddev()/Math.pow(this.arr.length, .5);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+        return mean() + 1.96 * stddev()/Math.pow(this.arr.length, .5);
+    }
+
+   // test client (see below)
+   public static void main(String[] args){
+
+    int N = Integer.parseInt(args[0]);
+    int Trials = Integer.parseInt(args[1]);
+
+    PercolationStats stats = new PercolationStats(N, Trials);
+
+    double mean         = stats.mean();
+    double stddev       = stats.stddev();
+    double confidenceLo = stats.confidenceLo();
+    double confidenceHi = stats.confidenceHi();
+
+    System.out.printf("mean:                    %f\n", mean);
+    System.out.printf("stddev:                  %f\n", stddev);
+    System.out.printf("95%% confidence interval: %s, %s", confidenceLo, confidenceHi);
+    System.out.print("\n");
+   }
+
+}
